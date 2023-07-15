fe25155c1c0f02d7037dd2b87973cc2d68682f88
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2023-07-15 19:51:08 +0200

    docs: Add comment to Message component

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,5 +1,12 @@
 import styles from "./Message.module.css";
 
+/**
+ * The Message component is a React component that renders a paragraph element with a greeting emoji
+ * and a message.
+ * @returns The Message component is returning a paragraph element with a class name of "message".
+ * Inside the paragraph, there is a span element with a role of "img" and an emoji of waving hand (👋),
+ * followed by the value of the "message" prop.
+ */
 function Message({ message }) {
   return (
     <p className={styles.message}>
