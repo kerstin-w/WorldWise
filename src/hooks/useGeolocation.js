ff162dddfd5f5952520d83df60e66100a8de1fc8
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2023-07-16 21:28:22 +0200

    docs: Add comment to useGeolocation Hook

--- src/hooks/useGeolocation.js
+++ src/hooks/useGeolocation.js
@@ -1,5 +1,13 @@
 import { useState } from "react";
 
+/**
+ * The `useGeolocation` function is a custom hook in JavaScript that allows you to retrieve the user's
+ * current position using the browser's geolocation API.
+ * @param [defaultPosition=null] - The defaultPosition parameter is an optional parameter that allows
+ * you to set a default position for the geolocation. If no defaultPosition is provided, it will be set
+ * to null.
+ * @returns The function `useGeolocation` returns an object with the following properties and methods:
+ */
 export function useGeolocation(defaultPosition = null) {
   const [isLoading, setIsLoading] = useState(false);
   const [position, setPosition] = useState(defaultPosition);
