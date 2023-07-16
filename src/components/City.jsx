b2d35f016c95a5e686a22067638e4d22869e5110
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2023-07-16 19:59:40 +0200

    feat: Add fetching the actual city data

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,5 +1,9 @@
-import { useParams, useSearchParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+
 import styles from "./City.module.css";
+import { useCities } from "../contexts/CitiesContext";
+import Spinner from "./Spinner";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -11,29 +15,16 @@ const formatDate = (date) =>
 
 function City() {
   const { id } = useParams();
-  const [searchParams, setSearchParams] = useSearchParams();
+  const { getCity, currentCity, isLoading } = useCities();
 
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
-  // TEMP DATA
-  const currentCity = {
-    cityName: "Lisbon",
-    emoji: "🇵🇹",
-    date: "2027-10-31T15:59:59.138Z",
-    notes: "My favorite city so far!",
-  };
+  useEffect(() => {
+    getCity(id);
+  }, [id]);
 
   const { cityName, emoji, date, notes } = currentCity;
-  return (
-    <>
-      <h1>City {id}</h1>
-      <p>
-        Position: {lat}, {lng}
-      </p>
-      ;
-    </>
-  );
-  /* 
+
+  if (isLoading) return <Spinner />;
+
   return (
     <div className={styles.city}>
       <div className={styles.row}>
@@ -42,19 +33,16 @@ function City() {
           <span>{emoji}</span> {cityName}
         </h3>
       </div>
-
       <div className={styles.row}>
         <h6>You went to {cityName} on</h6>
         <p>{formatDate(date || null)}</p>
       </div>
-
       {notes && (
         <div className={styles.row}>
           <h6>Your notes</h6>
           <p>{notes}</p>
         </div>
       )}
-
       <div className={styles.row}>
         <h6>Learn more</h6>
         <a
@@ -65,12 +53,9 @@ function City() {
           Check out {cityName} on Wikipedia &rarr;
         </a>
       </div>
-
-      <div>
-        <ButtonBack />
-      </div>
+      <div></div>
     </div>
-  );*/
+  );
 }
 
 export default City;
